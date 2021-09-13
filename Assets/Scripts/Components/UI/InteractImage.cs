2e963617a42d78d1b9504089fef355196ef1c86b
Alexey Kavin <smillerlenatderirinc@gmail.com>
2021-09-13 08:07:45 +0300

    Фикс иногда появляющейся руки вне интеракта

--- Assets/Scripts/Components/UI/InteractImage.cs
+++ Assets/Scripts/Components/UI/InteractImage.cs
@@ -38,11 +38,14 @@ namespace Components.UI
         private float currentX;
         private float currentY;
 
+        private bool currentState = false;
+
         public static UnityAction<bool> ShowAction() => m_showAction;
 
         private void onInteract(bool state)
         {
-            currentY = state ? showTransform.position.y : hideTransform.position.y;
+            currentState = state;
+            currentY = currentState ? showTransform.position.y : hideTransform.position.y;
         }
 
         private void Awake()
@@ -74,15 +77,16 @@ namespace Components.UI
 
             // Анимация рук при движении мыши
             inputX = Mathf.Lerp(inputX, -Input.GetAxisRaw("Mouse X") * inputXSensitivity, inputXAlpha);
-            inputY = Mathf.Lerp(inputY, -Input.GetAxisRaw("Mouse Y") * inputYSensitivity, inputYAlpha);
+            inputY = currentState ? Mathf.Lerp(inputY, -Input.GetAxisRaw("Mouse Y") * inputYSensitivity, inputYAlpha) : Mathf.Lerp(inputY, 0, inputYAlpha);
 
             // Анимация интеракта
             x = Mathf.Lerp(x, currentX, animationXAlpha) + inputX;
             y = Mathf.Lerp(y, currentY, animationYAlpha) + inputY;
 
             // Ограничение позиции руки, чтобы игроку не было видно нижней границы картинки
-            x = Mathf.Clamp(x, currentX -clampValueX, currentX +clampValueX);
-            y = Mathf.Clamp(y, hideTransform.position.y -clampValueY, showTransform.position.y +clampValueY);
+            x = Mathf.Clamp(x, currentX - clampValueX, currentX + clampValueX);
+            y = Mathf.Clamp(y, hideTransform.position.y - clampValueY, showTransform.position.y + clampValueY);
+            
 
             var result = new Vector2(x, y);
 
